4fa678ba7841d2d48e962177a853ae73a13ac9bb
TomatoS0uP85 <faizabror85@student.uns.ac.id>
2025-01-25 13:09:27 +0700

    removed news from about us and policy page

--- app/Views/pages/about_page.php
+++ app/Views/pages/about_page.php
@@ -2,40 +2,13 @@
 
 <?= $this->section('content'); ?>
 
-<div id="newsCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <?php $isActive = true; ?>
-        <?php foreach ($news as $item): ?>
-            <div class="carousel-item <?= $isActive ? 'active' : ''; ?>">
-                <img src="<?= base_url('uploads/news/' . esc($item['image_url'])); ?>" style="width: 100%; object-fit: cover; height: 500px; filter: brightness(0.6);" alt="<?= esc($item['title']); ?>">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5><?= esc($item['title']); ?></h5>
-                    <p><?= esc($item['description']); ?></p>
-                    <a href="/<?= esc($item['id']); ?>" class="btn btn-primary">Selengkapnya</a>
-                </div>
-            </div>
-            <?php $isActive = false; ?>
-        <?php endforeach; ?>
-    </div>
-
-    <main id="main">
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#newsCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden"></span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#newsCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden"></span>
-    </button>
+<div class="page-header d-flex align-items-end justify-content-center" style="height: 200px; background-color: #37517e">
+    <h1 class="section-title text-white">TENTANG KAMI</h1>
 </div>
 
+<main id="main">
     <!-- ======= Term of Service Section ======= -->
     <section id="about" class="about">
-        <div class="container">
-          <h2 class="section-title">TENTANG KAMI</h2>
-        </div>
-
         <div class="container">
         <p>CV Enuma Technology adalah perusahaan yang bergerak di bidang teknologi informasi, melayani kebutuhan bisnis melalui layanan konsultasi IT, perancangan sistem, pengembangan produk, dan pengelolaan produk. Berdiri dengan tujuan membantu bisnis berkembang di era digital, kami berkomitmen untuk memberikan solusi inovatif dan berkualitas tinggi.</p>
         <h3>Visi</h3>
